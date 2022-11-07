ef60f0bd7351ced094d72965bc387ef0ab77d648
hekmatelganzory <hekmat.elganzory30@gmail.com>
2022-11-07 11:27:02 +0200

    multiple line comment

--- Tasks_List.py
+++ Tasks_List.py
@@ -1,4 +1,6 @@
-"""Make a list containing the names of your friends, and it contains at least 5 names. What is required in the first and second line is to print the name of the first friend in the list in two ways, then in the third and fourth line you print the name of the last friend in the list in two ways"""
+"""Make a list containing the names of your friends, and it contains at least 5 names
+. What is required in the first and second line is to print the name of the first friend in the list in two ways,
+ then in the third and fourth line you print the name of the last friend in the list in two ways"""
 
 friends = ["Mazen", "Ahmed", "Sayed", "Ali", "Mahmoud"]
 print(friends[0])  
