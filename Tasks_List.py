177cf752ceba1181c55d2aa1dc1193c9673326f2
hekmatelganzory <hekmat.elganzory30@gmail.com>
2022-11-07 10:57:35 +0200

    number one

---
+++ Tasks_List.py
@@ -0,0 +1,8 @@
+"""Make a list containing the names of your friends, and it contains at least 5 names. What is required in the first and second line is to print the name of the first friend in the list in two ways, then in the third and fourth line you print the name of the last friend in the list in two ways"""
+
+friends = ["Mazen", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+print(friends[0])  
+print(friends.pop(0))  
+print(friends[-1])  
+print(friends.pop(-1)) 
+
